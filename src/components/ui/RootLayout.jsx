d82dd0bc8955eed0214d97b2b27e4c7c9aeccedf
jocabello <jose.icc87@gmail.com>
2023-07-13 14:17:29 -0400

    adds responsive navbar

--- src/components/ui/RootLayout.jsx
+++ src/components/ui/RootLayout.jsx
@@ -1,16 +1,16 @@
 import { Outlet } from "react-router-dom"
 import Footer from "./Footer"
-import Navbar from "./Navbar"
+import NavbarWithCTAButton from "./Navbar"
 
 
 export default function RootLayout() {
   return (
     <div>
-        <Navbar />
+        <NavbarWithCTAButton />
         <main>
             <Outlet />
-            <Footer />
         </main>
+        <Footer />
     </div>
   )
 }
