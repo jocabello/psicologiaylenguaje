1f1dcbb72c659c373cf587f267f4489216266d9d
jocabello <jose.icc87@gmail.com>
2023-08-23 13:05:40 -0400

    fix: padding for buttons in HomePage

--- src/components/pages/site-home/HomePage.jsx
+++ src/components/pages/site-home/HomePage.jsx
@@ -8,7 +8,7 @@ import CardsComponent from './CardsComponent';
 export default function HomePage() {
   return (
     <div className='xl:px-28'>
-      
+
       <div className='h-96 px-10 mt-3 md:mt-8 xl:mt-8 xl:mb-5'>
         <Carousel slideInterval={7000} className=''>
           <img
@@ -38,7 +38,7 @@ export default function HomePage() {
 
       <CardsComponent />
 
-      <div className='xl:pt-20 grid place-content-center'>
+      <div className='pt-10 xl:pt-20 grid place-content-center'>
         <NavLink to="contacto">
           <Button size="xl" gradientDuoTone="purpleToPink">¡Contáctanos y reserva tu hora!</Button>
         </NavLink>
@@ -47,7 +47,7 @@ export default function HomePage() {
       <div className="xl:py-10 px-10 xl:mb-10 text-center mt-10 mb-5 ">
         <p className="mb-5 text-3xl text-gray-900 sm:text-4xl">Visita nuestra sección de preguntas frecuentes</p>
         <p className="mx-auto max-w-7xl items-center  mb-10 text-2xl italic font-medium text-gray-600 dark:text-white">¿Cuál es el abordaje terapéutico de la psicología cabalística ? ¿Qué es un trastorno fonológico? ¿Qué es la consejería? ¿Qué esperar del lenguaje a los tres años?</p>
-        <NavLink className="grid place-content-center content-center" to="preguntasyrespuestas">
+        <NavLink className="pb-5 grid place-content-center content-center" to="preguntasyrespuestas">
           <Button size="md" pill gradientDuoTone="redToYellow">Conoce más<HiOutlineArrowRight className="ml-2 h-5 w-5" /></Button>
         </NavLink>
       </div>
