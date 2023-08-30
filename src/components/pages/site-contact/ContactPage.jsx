11efcd592903f34e02e35dd33b110bc7a2af23a4
jocabello <jose.icc87@gmail.com>
2023-08-30 14:28:35 -0400

    fix: contactPage address

--- src/components/pages/site-contact/ContactPage.jsx
+++ src/components/pages/site-contact/ContactPage.jsx
@@ -7,7 +7,7 @@ export default function ContactPage() {
 
       <div className='h-96 px-10 mb-3'>
         <iframe 
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1617.3508863900329!2d-70.65350525500652!3d-33.436652743121414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5a4b8ea475b%3A0xff5cd7285775682c!2sServicios%20Informaticos%20Para%20El%20Comercio%20Limitada!5e0!3m2!1sen!2scl!4v1689633419190!5m2!1sen!2scl" 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3330.1617773972835!2d-70.60069862337431!3d-33.41902639590513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662cf6b2945b099%3A0xc70d7b8e0e1195b4!2sCallao%202970%2C%20Las%20Condes%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1sen!2scl!4v1693419905938!5m2!1sen!2scl" 
           width="100%"
           height="100%"
           style={{border:0}} 
